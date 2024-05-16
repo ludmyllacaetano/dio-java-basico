87348de2e03bb1bfadc52bc928f7b0d9f2ce1a37
ludmylla <ludmyllacaetano1@gmail.com>
2024-05-16 17:54:30 -0300

    Adição das funcionalidades

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,9 +1,29 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+
+import java.util.Scanner;
+
+public class ContaTerminal {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // TODO: Conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Exibir as mensagens para o usuário
+        // Obter pela scanner os valores digitados no terminal
+
+        System.out.print("Digite seu nome: ");
+        String cliente = scanner.nextLine();
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.print("Digite o número da conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.print("Digite o saldo: ");
+        float saldo = scanner.nextFloat();
+
+        // Exibir a mensagem conta criada
+        System.out.println("Olá " + cliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo "+ saldo + " já está disponível para saque.");
     }
 }
